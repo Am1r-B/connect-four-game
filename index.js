27beea608a971c8284bcc39fcca55702879f3d9e
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-04-29 23:23:33 +0330

    Add ability to place discs

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (let i = 0; i < squares.length; i++) {
     squares[i].onclick = () => {
-      alert("You have clicked square " + i);
+      // if the square below your current square is taken, you can go on top of it
+      if (
+        squares[i + 7].classList.contains("taken") &&
+        !squares[i].classList.contains("taken")
+      ) {
+        if (currentPlayer === 1) {
+          squares[i].classList.add("taken");
+          squares[i].classList.add("player-one");
+          currentPlayer = 2;
+          currentPlayerDisplay.innerHTML = currentPlayer;
+        } else if (currentPlayer === 2) {
+          squares[i].classList.add("taken");
+          squares[i].classList.add("player-two");
+          currentPlayer = 1;
+          currentPlayerDisplay.innerHTML = currentPlayer;
+        }
+      } else alert("cant go here");
     };
   }
 });
